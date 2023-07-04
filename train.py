546819332ddb3a4abf9923c1adf8e310dd3bdb7a
st-meiji <shin.takasuka@gmail.com>
2023-07-04 12:37:02 +0900

    new change

--- train.py
+++ train.py
@@ -6,20 +6,9 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import os
 from matplotlib import animation
+import utils
 
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]= "true"
-
-def display_frames_as_gif(frames):
-    plt.figure(figsize=(frames[0].shape[1]/72.0, frames[0].shape[0]/72.0), dpi=72)
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-
-    def animate(i):
-        patch.set_data(frames[i])
-
-    anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=50)
-    anim.save('3_2movie_cartpole.gif')  # 動画を保存
-
 
 # Configuration parameters for the whole setup
 seed = 42
@@ -139,4 +128,4 @@ while True:  # Run until solved
         break
 
 #print(frames[0].shape)
-display_frames_as_gif(frames)
+utils.display_frames_as_gif(frames)
